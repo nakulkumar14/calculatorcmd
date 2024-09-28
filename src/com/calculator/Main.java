50196a40e1598775a02de81ea4994ecb1476a84c
Nakul Kumar <nakulkumar14.09@gmail.com>
2024-09-28 18:08:22 +0530

    Simple calculator app - 2

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -3,6 +3,7 @@ package com.calculator;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         // addition, subtraction, multiplication and division.
         Scanner input = new Scanner(System.in);
@@ -12,41 +13,68 @@ public class Main {
                 2. Subtraction
                 3. Multiplication
                 4. Division
+                5. Clear last stored result
                 0. Exit
                 """;
         int choice = -1;
+        boolean reset = true;
+        int result = 0;
         do {
             System.out.println(message);
             choice = input.nextInt();
             int a = 0, b = 0;
             switch (choice) {
                 case 1:
-                    a = input.nextInt();
+                    if (reset) {
+                        a = input.nextInt();
+                        reset = false;
+                    } else {
+                        a = result;
+                    }
                     b = input.nextInt();
-                    int sum = a + b;
-                    System.out.println("Sum of a : " + a + " and b : " + b + " = " + sum);
+                    result = a + b;
+                    System.out.println("Sum of a : " + a + " and b : " + b + " = " + result);
                     break;
                 case 2:
-                    a = input.nextInt();
+                    if (reset) {
+                        a = input.nextInt();
+                        reset = false;
+                    } else{
+                        a = result;
+                    }
                     b = input.nextInt();
-                    int sub = a - b;
-                    System.out.println("Subtraction of a : " + a + " and b : " + b + " = " + sub);
+                    result = a - b;
+                    System.out.println("Subtraction of a : " + a + " and b : " + b + " = " + result);
                     break;
                 case 3:
-                   a = input.nextInt();
+                    if (reset) {
+                        a = input.nextInt();
+                        reset = false;
+                    } else{
+                        a = result;
+                    }
                    b = input.nextInt();
-                   int mul = a * b;
-                   System.out.println("Multiplication of a : " + a + " and b : " + b + " = " + mul);
+                   result = a * b;
+                   System.out.println("Multiplication of a : " + a + " and b : " + b + " = " + result);
                     break;
                 case 4:
-                    a = input.nextInt();
+                    if (reset) {
+                        a = input.nextInt();
+                        reset = false;
+                    } else{
+                        a = result;
+                    }
                     b = input.nextInt();
                     if (b == 0) {
                         System.out.println("Division by zero not allowed!!");
                     } else {
-                        int div = a / b;
-                        System.out.println("Division of a : " + a + " and b : " + b + " = " + div);
+                        result = a / b;
+                        System.out.println("Division of a : " + a + " and b : " + b + " = " + result);
                     }
+                    break;
+                case 5:
+                    result = 0;
+                    reset = true;
                     break;
                 default:
                     if (choice != 0) {
